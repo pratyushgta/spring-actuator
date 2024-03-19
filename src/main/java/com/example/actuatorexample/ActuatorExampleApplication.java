87e086a5daaa1dbaff6a503ec3db087c10dba313
Pratyush Kumar <pratyushgta@gmail.com>
2024-03-19 23:18:01 +0530

    Initial commit

---
+++ src/main/java/com/example/actuatorexample/ActuatorExampleApplication.java
@@ -0,0 +1,20 @@
+/**
+ * Main class of API Gateway
+ * MSA-E6
+ *
+ * @author Pratyush Kumar (github.com/pratyushgta)
+ */
+
+package com.example.actuatorexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActuatorExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActuatorExampleApplication.class, args);
+	}
+
+}
